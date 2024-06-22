3bb55acdefc40c678af8776cd3270ef040c039d4
Taylor Ng <taylormng95@gmail.com>
2024-06-22 11:27:10 -0400

    completes fibonacci lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    lst = [0, 1]
+    if length < 3:
+        lst = lst[0:length]
+    else:
+        for i in range(length - 2):
+            lst.append(lst[-1] + lst[-2])
+    print(lst)
+
+def add_number():
+    lst.append(lst[-1] + lst[-2])
+    print(lst)
+
+print_fibonacci(2)
